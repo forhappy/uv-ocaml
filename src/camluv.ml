a934c6026ec00b2df1d46827b255649ebc95d107
forhappy <haipingf@gmail.com>
2014-01-05 13:14:32 +0800

    add camluv.ml and camluv.mli

---
+++ src/camluv.ml
@@ -0,0 +1,26 @@
+type loop
+
+type handle
+
+type uv_run_mode =
+    UV_RUN_DEFAULT
+  | UV_RUN_ONCE
+  | UV_RUN_NOWAIT
+
+
+type uv_walk_cb = handle -> string -> unit
+
+module Loop =
+  struct
+    external create: unit->loop = "camluv_loop_new"
+    external default: unit->loop = "camluv_loop_default"
+    external delete: loop -> unit = "camluv_loop_delete"
+    external run: loop -> uv_run_mode -> int32 = "camluv_loop_run"
+    external stop: loop -> unit = "camluv_loop_stop"
+    external now: loop -> int64 = "camluv_loop_now"
+    external update_time: loop -> unit = "camluv_loop_update_time"
+    external backend_fd: loop -> int32 = "camluv_loop_backend_fd"
+    external backend_timeout: loop -> int32 = "camluv_loop_backend_timeout"
+    external walk: loop -> uv_walk_cb -> string -> unit = "camluv_loop_walk"
+  end
+
