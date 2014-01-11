a9d3efd18d8e922d2092eccd5b1bc9bbd9c9c4de
forhappy <haipingf@gmail.com>
2014-01-11 19:13:00 +0800

    add fs_poll_basic example.

---
+++ examples/fs_poll_basic.ml
@@ -0,0 +1,13 @@
+open Camluv
+
+let fs_poll_cb handle status prev curr =
+    print_string "Something happened.\n";;
+
+
+let loop = Loop.default ();;
+let fs_poll = FsPoll.init loop;;
+
+FsPoll.start fs_poll fs_poll_cb "/tmp/xxx" 1;;
+
+let rc = Loop.run loop UV_RUN_DEFAULT;;
+
