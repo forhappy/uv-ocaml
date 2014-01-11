da9e141d6bf60ebd1db67c4f484b84f6f3dd03a7
forhappy <haipingf@gmail.com>
2014-01-11 23:38:54 +0800

    rewrite fs_poll_basic.ml example.

--- examples/fs_poll_basic.ml
+++ examples/fs_poll_basic.ml
@@ -3,11 +3,8 @@ open Camluv
 let fs_poll_cb handle status prev curr =
     Printf.printf "Something happened.%!\n";;
 
-
-let loop = Loop.default ();;
-let fs_poll = FsPoll.init loop;;
-
-FsPoll.start fs_poll fs_poll_cb "/tmp/xxx" 1;;
-
-let rc = Loop.run loop UV_RUN_DEFAULT;;
+let loop = Loop.default () in
+  let fs_poll = FsPoll.init loop in
+    let _ = FsPoll.start fs_poll fs_poll_cb "/tmp/xxx" 1 in
+      Loop.run loop UV_RUN_DEFAULT;;
 
