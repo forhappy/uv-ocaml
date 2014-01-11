7e6cdc440a5af4a01334eb496a6f6a1afe35f0bd
forhappy <haipingf@gmail.com>
2014-01-11 23:25:24 +0800

    rewrite fs_event_basic.ml code.

--- examples/fs_event_basic.ml
+++ examples/fs_event_basic.ml
@@ -3,10 +3,8 @@ open Camluv
 let fs_event_cb handle filename event status =
     Printf.printf "Event %d happened on file: %s with status %d.%!\n" (event) (filename) (status);;
 
-let loop = Loop.default ();;
-let fs_event = FsEvent.init loop;;
-
-FsEvent.start fs_event fs_event_cb "/tmp/xxx" 0;;
-
-let rc = Loop.run loop UV_RUN_DEFAULT;;
+let loop = Loop.default () in
+  let fs_event = FsEvent.init loop in
+    let _ = FsEvent.start fs_event fs_event_cb "/tmp/xxx" 0 in
+      Loop.run loop UV_RUN_DEFAULT;;
 
